0d1376382d45298d5909aef3c127287fbd47b34d
vishnu <vishnuajith@gmail.com>
2025-06-19 20:56:41 +0100

    saxpy fixed

--- benchmark/saxpy/saxpy.cpp
+++ benchmark/saxpy/saxpy.cpp
@@ -1,33 +1,22 @@
-/* Basic sacalr multiplication and vector addition*/
+/* Basic scalar multiplication and vector addition*/
 
 
-#define NTAPS 32
+#define SIZE 32
 
 
-float output[NTAPS];
+float output[SIZE];
 float alpha = -1.5;
-float X[NTAPS] = {0.25, 1.50, 3.75, -2.25, 0.50, 0.75, -3.00, 1.25,
-    0.25, 1.50, 3.75, -2.25, 0.50, 0.75, -3.00, 1.25,
-    0.25, 1.50, 3.75, -2.25, 0.50, 0.75, -3.00, 1.25,
-    0.25, 1.50, 3.75, -2.25, 0.50, 0.75, -3.00, 1.25};
+float X[SIZE];
+float Y[SIZE];
 
-float Y[NTAPS] = {0.35, -1.50, 3.75, -2.25, 0.50, 0.75, -3.00, 1.25,
-        0.25, 1.50, 3.75, -2.25, 0.50, 0.75, -3.00, 1.25,
-        0.25, 1.50, 3.75, -2.25, 0.50, 0.75, -3.00, 1.25,
-        0.25, 1.50, 3.75, -2.25, 0.50, 0.75, -3.00, 1.25};
 
 void kernel(float X[], float Y[], float alpha, float output[]);
 
 int main()
 {
-
-//  input_dsp (input, NTAPS, 0);
 float a = 3.9;
   kernel(X, Y, a, output);
 
-//  output_dsp (input, NTAPS, 0);
-//  output_dsp (coefficients, NTAPS, 0);
-//  output_dsp (output, NTAPS, 0);
   return 0;
 }
 
@@ -38,7 +27,7 @@ scalar multiplication of alpha * X followed by vector addition of Y.
 {
 int i;
 
-for (i = 0; i < NTAPS; ++i) {
+for (i = 0; i < SIZE; ++i) {
 output[i] = alpha * X[i] + Y[i];
 }
 
